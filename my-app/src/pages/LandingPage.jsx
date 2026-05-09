08f8253eb3594eae23174e3003f03b4eeecba7c9
sol <solomongetahun20@gmail.com>
2026-05-09 12:37:19 +0300

    landing demo

--- my-app/src/pages/LandingPage.jsx
+++ my-app/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 
-
+import { Link } from "react-router"
 function LandingPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Link to="/student/signup">student</Link> <br />
+            <Link to="/teacher/signup">teacher</Link> <br />
+            <Link to="/parent/signup">parent</Link> <br />
+        </div>
+    )
 }
 
-export  {LandingPage}
+export { LandingPage }
